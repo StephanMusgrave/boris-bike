2877369b10c1935f63dc788a8c3984c083a7ad46
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-04-14 11:50:42 +0100

    added bike.rb to lib

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,13 @@
-require "boris-bike"
+require "bike"
 
+# we're describing the functionality of the specific class, Bike
 describe "boris-bike" do
+  # this is a specific feature (behaviour)
+  # that we expect to be present
+  it "should not be broken after we create it" do
+    bike = Bike.new
+    expect(bike).not_to be_broken
+  end
 
 
 
