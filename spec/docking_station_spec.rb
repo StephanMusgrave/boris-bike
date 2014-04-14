f23104b4cbd5f4e8deafc37dd5eb17075b87126c
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-04-14 14:05:56 +0100

    refactored docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,9 @@ describe DockingStation do
   let(:bike) { Bike.new}
   let(:station) { DockingStation.new }
   let(:station) { DockingStation.new(:capacity => 20)}
+  def fill_station(station)
+    20.times { station.dock(Bike.new) }
+  end
  
   it "should be able to accept a bike" do
     bike = Bike.new
@@ -20,14 +23,14 @@ describe DockingStation do
 
   it "should know when it's full" do
     expect(station).not_to be_full
-    20.times { station.dock(Bike.new) }
+    fill_station station
     expect(station).to be_full
   end
 
   it "should not accept a bike if it's full" do
-    20.times { station.dock(Bike.new) }
+    fill_station station
     expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
   end
-  
+
 
 end
