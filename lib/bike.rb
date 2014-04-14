951de18e1a8afa4aa8dc8e270e4b975fcd83876c
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-04-14 12:06:57 +0100

    added initialise and break! methods to bike.rb

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,14 @@
 class Bike
-	def broken?
-		false
-	end
+  def initialise
+    @broken = false
+  end
+
+  def broken?
+    @broken
+  end
+
+  def break!
+    @broken = true
+  end
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,10 +4,18 @@ require "bike"
 describe "boris-bike" do
   # this is a specific feature (behaviour)
   # that we expect to be present
+  
   it "should not be broken after we create it" do
     bike = Bike.new
     expect(bike).not_to be_broken
   end
+
+  it "should be able to break" do
+    bike = Bike.new
+    bike.break!
+    expect(bike).to be_broken
+  end
+
 
 
 
