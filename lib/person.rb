ebbb8190f884631966ec363bb04e04c7e3e67578
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-04-18 15:35:32 +0100

    adds peson test and code

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+
+	def initialize(bike=nil)
+		@bike = bike
+	end
+
+	def has_bike?
+		!@bike == nil?
+	end
+
+	def fall_down
+		@bike.break!
+	end
+
+	def rent_bike_from(station)
+		@bike = station.release_bike
+		self
+	end
+
+	def return_bike_to(station)
+		 station.dock(@bike)
+	end
+
+end
---
+++ spec/person_spec.rb
@@ -0,0 +1,43 @@
+require 'person'
+
+describe Person do
+	let(:person) { Person.new }
+	let(:person_with_bike) { Person.new(:bike)}
+
+	it 'Person has no bike when created' do 
+		expect(person).not_to have_bike
+	end
+
+	it 'can have a bike' do
+		expect(person_with_bike).to have_bike
+	end
+
+	it 'can have a bike accident' do
+		bike = double :bike
+		person = Person.new(bike)
+		expect(bike).to receive(:break!)
+		person.fall_down 
+	end
+
+	it 'can rent a bike from a docking station' do
+		person = Person.new
+		station = double :station, release_bike: :bike
+		expect(person.rent_bike_from(station)).to have_bike
+	end
+
+	it 'returns a bike to a docking station' do
+		station = double :station
+		expect(station).to receive(:dock).with(:bike)
+		person_with_bike.return_bike_to(station)
+	end
+
+	it 'has no bike after docking' do
+		station = double :station, dock: ''
+		expect(person).not_to have_bike
+		person_with_bike.return_bike_to(station)
+	end
+
+
+
+
+end
